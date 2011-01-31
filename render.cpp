87bd49a009aae5d8b3205735b69c8319fb162a43
Bayley Wang <wang9xb17@gmail.com>
2011-01-31 02:27:21 -0500

    fixed vec.cpp

--- render.cpp
+++ render.cpp
@@ -3,7 +3,13 @@
 
 void render()
 {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(60.0, 1.0, 1.0, 30.0);
+	glMatrixMode(GL_MODELVIEW);
+	gluLookAt(3.0, 3.0, 3.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 	GLUquadric* quad = gluNewQuadric();
 	glColor3f(1.0, 1.0, 1.0);
-	gluSphere(quad, 0.1, 10, 10);
+	gluSphere(quad, 1.0, 10, 10);
 }

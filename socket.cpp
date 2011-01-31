ce1391cb82303aef18fe879732fac0cc40cef61c
Michael Cohen <gnurdux@gmail.com>
2011-01-31 05:48:05 -0500

    Fixed

--- socket.cpp
+++ socket.cpp
@@ -28,5 +28,5 @@ bool Socket::receive(char* stuff, int size) {
 bool Socket::hasRemaining() {
 	char c;
 	int val = recv(socket, &c, 1, MSG_DONTWAIT|MSG_PEEK);
-	return (val != 1 || (errno != EAGAIN && errno != EWOULDBLOCK));
+	return (val != -1 || (errno != EAGAIN && errno != EWOULDBLOCK));
 }

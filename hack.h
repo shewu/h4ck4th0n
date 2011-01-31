107e03b0301aa1a9d121a54e98fa4d32a147ce4b
Bayley Wang <bayley@glow-up-bulb.(none)>
2011-01-30 23:25:34 -0500

    Updated hack.h

--- hack.h
+++ hack.h
@@ -2,18 +2,60 @@ class Vector2D
 {
   public:
 		float x, y;
+		Vector2D() {x= y = 0;}
+		Vector2D(float a, float b) {x = a; y = b;}
+		Vector2D operator+(const Vector2D);
+		Vector2D operator-(const Vector2D);
+		float operator*(const Vector2D);
 };
+
+Vector2D Vector2D::operator+(const Vector2D v)
+{
+  return Vector2D(x + v.x, y + v.y);
+}
+
+Vector2D Vector2D::operator-(const Vector2D v)
+{
+  return Vector2D(x - v.x, y - v.y);
+}
+
+float Vector2D::operator*(const Vector2D v)
+{
+	return x * v.x + y * v.y;
+}
 
 class Vector3D
 {
 	public:
 		float x, y, z;
+		Vector3D() {x = y = 0;}
+		Vector3D(float a, float b, float c) {x = a; y = b; z = c;}
+		Vector3D(Vector2D v) {x = v.x; y = v.y; z = 0;}
+		Vector3D operator+(const Vector3D);
+		Vector3D operator-(const Vector3D);
+		float operator*(const Vector3D);
 };
+
+Vector3D Vector3D::operator+(const Vector3D v)
+{
+	return Vector3D(x + v.x, y + v.y, z + v.z);
+}
+
+Vector3D Vector3D::operator-(const Vector3D v)
+{
+	return Vector3D(x - v.x, y - v.y, z - v.z);
+}
+
+float Vector3D::operator*(const Vector3D v)
+{
+	return x * v.x + y * v.y + z * v.z;
+}
 
 class Color
 {
 	public:
 		unsigned char r, g, b;
+		Color(unsigned char x, unsigned char y, unsigned char z) {r = a; g = y; b = z;} 
 };
 
 class Object

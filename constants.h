b888760fa50c3d191bea639aa991cbbd7b256ffc
Michael Cohen <gnurdux@gmail.com>
2011-02-22 17:07:36 -0500

    No BSPs

--- constants.h
+++ constants.h
@@ -1,8 +1,8 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-#define WIDTH 1024
-#define HEIGHT 768
+#define WIDTH 640
+#define HEIGHT 480
 #define MIN_X -30.0f
 #define MAX_X 30.0f
 #define MIN_Y -30.0f

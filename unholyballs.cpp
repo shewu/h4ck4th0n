aa21a9faaac3769ff2cc83f0b7a2ed9d3de975b8
bhamrick <brian.c.hamrick@gmail.com>
2011-04-03 22:49:50 -0400

    Catch more errors in initialization

--- unholyballs.cpp
+++ unholyballs.cpp
@@ -41,6 +41,11 @@ void initVideo()
 		screen = SDL_SetVideoMode(WIDTH, HEIGHT, 24, SDL_OPENGL | SDL_FULLSCREEN);
 	else
 		screen = SDL_SetVideoMode(WIDTH, HEIGHT, 24, SDL_OPENGL);
+	
+	if(screen == NULL) {
+		printf("SDL_SetVideoMode failed: %s\n",SDL_GetError());
+	}
+
 	SDL_ShowCursor(false);
 	SDL_WM_GrabInput(SDL_GRAB_OFF);
 	
@@ -274,22 +279,8 @@ int main(int argc, char* argv[])
 		if ((++count)%100 == 0) {
 			int time = SDL_GetTicks();
 			float fps = (100*1000.)/(time - oldTime);
-			printf("\b\b\b\b\b\b\b\b\b");
-			if(fps < 10) {
-				cout << " ";
-			}
-			if(fps < 100) {
-				cout << " ";
-			}
-			if(fps < 1000) {
-				cout << " ";
-			}
-			if(fps < 10000) {
-				cout << " ";
-			}
-			if(fps < 100000) {
-				cout << " ";
-			}
+			printf("\r");
+			cout.width(6);
 			cout << (int)fps << "fps";
 			oldTime = time;
 			fflush(stdout);

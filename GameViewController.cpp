8d0daf48cad1121cf66551263661395025fd58c7
Sherry Wu <shewu19@gmail.com>
2011-07-16 11:37:21 -0700

    added function prototypes in game view controller

--- GameViewController.cpp
+++ GameViewController.cpp
@@ -2,5 +2,21 @@
 
 class GameViewController : public HBViewController {
 	public:
+		GameViewController();
+		~GameViewController();
+		void process();
+		void render();
 };
+
+GameViewController::GameViewController() {
+}
+
+GameViewController::~GameViewController() {
+}
+
+void GameViewController::process() {
+}
+
+void GameViewController::render() {
+}
 

e3773edb6a575f5cfc1c5a6244d862fa571c08ab
Sherry Wu <shewu19@gmail.com>
2011-07-16 10:34:13 -0700

    main view controller and one subclass, game view controller

---
+++ GameViewController.cpp
@@ -0,0 +1,6 @@
+#include "render.h"
+
+class GameViewController : public HBViewController {
+	public:
+};
+
---
+++ HBViewController.cpp
@@ -0,0 +1,6 @@
+class HBViewController {
+	public:
+		virtual void process() = 0;
+		virtual void render() = 0;
+};
+

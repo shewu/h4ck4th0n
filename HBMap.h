735f38ea983c191cae823b93347d304a9d12809b
Sherry Wu <shewu19@gmail.com>
2012-03-29 17:39:31 -0400

    documented public functions in HBMap

--- HBMap.h
+++ HBMap.h
@@ -8,40 +8,106 @@
 
 class HBMap {
     public:
+		/**
+		 * Empty constructor that does nothing.
+		 */
         HBMap() { }
+
+		/**
+		 * Creates an HBMap instance given the map filename.
+		 *
+		 * @throw ParseException when the parser encounters invalid syntax.
+		 */
         HBMap(std::string filename) {
             parse(filename);
         }
+
+		/**
+		 * Gets the width of the map.
+		 *
+		 * @return the width.
+		 */
         unsigned getWidth() {
             return width;
         }
+
+		/**
+		 * Gets the height of the map.
+		 *
+		 * @return the height.
+		 */
         unsigned getHeight() {
             return height;
         }
+
+		/**
+		 * Gets the name of the map.
+		 *
+		 * @return the name.
+		 */
         std::string& getName() {
             return mapName;
         }
+
+		/**
+		 * Gets the game modes of the map.
+		 *
+		 * @return the game modes in no particular order.
+		 */
         std::vector<GameMode>& getGameModes() {
             return modes;
         }
+
+		/**
+		 * Gets the team list and properties of the map.
+		 *
+		 * @return the team list containing Team objects in no particular
+		 * order.
+		 */
         std::vector<Team>& getTeams() {
             return teams;
         }
+
+		/**
+		 * Gets the spawns for a given team.
+		 *
+		 * @param team the team number for which to get spawns.
+		 *
+		 * @return the spawns for team.
+		 */
         std::vector<Obstacle>& getSpawnsForTeam(unsigned team) {
             if (team >= teams.size()) {
                 return spawns[0];
             }
             return spawns[team];
         }
+
+		/**
+		 * Gets the flags for a given team.
+		 *
+		 * @param team the team number for which to get flags.
+		 *
+		 * @return the flags for team.
+		 */
         std::vector<Spawn>& getFlagsForTeam(unsigned team) {
             if (team >= teams.size()) {
                 return flags[0];
             }
             return flags[team];
         }
+
+		/**
+		 * Gets the walls for the map.
+		 *
+		 * @return a list of walls.
+		 */
         std::vector<Obstacle>& getWalls() {
             return walls;
         }
+
+		/**
+		 * The maximum number of teams allowed in maps.
+		 */
         const static unsigned MAX_TEAMS = 10;
     private:
         void parse(std::string filename);

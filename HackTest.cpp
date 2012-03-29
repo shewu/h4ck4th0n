960f03146d5be0ced0a766d7aaa71427fb5fd966
Sherry Wu <shewu19@gmail.com>
2012-03-29 14:57:34 -0400

    forgot to remove HBMap crap from HackTest

--- HackTest.cpp
+++ HackTest.cpp
@@ -1,7 +1,6 @@
 #include <cstdio>
 
 #include "hack.h"
-#include "HBMap.h"
 
 #define TEST_EQ(d, x, y) \
 	do { \
@@ -19,7 +18,6 @@ using namespace std;
 
 void Vector2DTest();
 void Vector3DTest();
-void MapTest();
 
 int main()
 {
@@ -76,10 +74,5 @@ void Vector3DTest()
 	TEST_EQ("dot operator*", i*j, 0);
 	TEST_EQ("scalar multiply operator*", j*1.5, Vector3D(0,1.5,0));
 	return;
-}
-
-void MapTest() {
-    HBMap custom("custom.hbm");
-    TEST_TRUE("read good map", true);
 }
 

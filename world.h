f8361e71417b4ba3debd0f1c452e111ed0b2787c
Sherry Wu <shewu19@gmail.com>
2012-03-30 23:58:44 -0400

    added getters for the dimension of the world

--- world.h
+++ world.h
@@ -13,11 +13,53 @@ class World
 		std::string mapName;
         Color wallColor;
         HBMap worldMap;
+
 	public:
+        /**
+         * Default constructor for World given a map name from which to load
+         * the world.
+         *
+         * @param mapName the map file name. Defaults to "custom.hbm".
+         */
 		World(std::string mapName = "custom.hbm");
 		int spawn(int spawnl, int player, int flag);
+
+        /**
+         * Returns the minimum x coordinate of the world.
+         *
+         * @return the minimum x coordinate.
+         */
+        const float getMinX() const {
+            return worldMap.getWidth()/-2.0f;
+        }
+
+        /**
+         * Returns the maximum x coordinate of the world.
+         *
+         * @return the maximum x coordinate.
+         */
+        const float getMaxX() const {
+            return worldMap.getWidth()/2.0f;
+        }
+
+        /**
+         * Returns the minimum y coordinate of the world.
+         *
+         * @return the minimum y coordinate.
+         */
+        const float getMinY() const {
+            return worldMap.getHeight()/-2.0f;
+        }
+
+        /**
+         * Returns the maximum y coordinate of the world.
+         *
+         * @return the maximum y coordinate.
+         */
+        const float getMaxY() const {
+            return worldMap.getHeight()/2.0f;
+        }
 		
-		float minX, maxX, minY, maxY;
 		std::map<int, std::vector<Spawn> > spawns;
 		std::map<int, Object> objects;
 		std::vector<Light> lights;

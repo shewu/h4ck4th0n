373dba75b182530747df581cabb0f918a7bde5cf
shewu <shewu19@gmail.com>
2012-04-08 01:24:08 -0400

    added main function for test

--- tests/test.cpp
+++ tests/test.cpp
@@ -6,3 +6,8 @@ TEST (ExampleTest, SimpleStatements) {
     EXPECT_EQ(true, true);
 }
 
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+

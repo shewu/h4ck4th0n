db4fb3bd6f854513043bb3e3c4218daee891f4ab
Sherry Wu <shewu19@gmail.com>
2012-05-25 22:50:33 -0400

    messed around with tests; still do not work

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,13 +1,8 @@
 #include <gtest/gtest.h>
 
-TEST (ExampleTest, SimpleStatements) {
+TEST(ExampleTest, SimpleStatements) {
     EXPECT_TRUE(true);
     EXPECT_FALSE(false);
     EXPECT_EQ(true, true);
-}
-
-int main(int argc, char** argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
 }
 

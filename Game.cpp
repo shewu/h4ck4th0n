b72c89dd1432584f6d22234f6cfdc1463dc96207
Travis Hance <tjhance7@gmail.com>
2012-05-30 16:36:41 -0400

    fixed user input

--- Game.cpp
+++ Game.cpp
@@ -1,4 +1,4 @@
-#include "game.h"
+#include "Game.h"
 #include "constants.h"
 
 #include <cmath>
--- UserInput.h
+++ UserInput.h
@@ -4,25 +4,24 @@
 class UserInput {
 	private:
 		bool _left, _right, _up, _down;
+		float _theta;
 
 	public:
 		/**
 		 * Creates a UserInput object from the given input key inputs.
-		 * @param left True if the left arrow key is pressed down.
-		 * @param right True if the right arrow key is pressed down.
-		 * @param up True if the up arrow key is pressed down.
-		 * @param down True if the down arrow key is pressed down.
+		 * @param left True if the `left' button is pressed down.
+		 * @param right True if the `right' button is pressed down.
+		 * @param up True if the `up' button is pressed down.
+		 * @param down True if the `down' button is pressed down.
 		 */
-		UserInput(bool left, bool right, bool up, bool down) :
-		    _left(left), _right(right), _up(up), _down(down) { }
+		UserInput(bool left, bool right, bool up, bool down, float theta) :
+		    _left(left), _right(right), _up(up), _down(down), _theta(theta) { }
 
 		/**
-		 * @return the angle change rate indicated by the key input.
-		 */
-		float getAngleChangeRate();
-
-		/**
+		 * Returns the forward acceleration vector indicated by the key input; that is,
+		 * the acceleration of the player, where (0, 1) points in the direction the player
+		 * is currently facing.
 		 * @return the forward acceleration indicated by the key input.
 		 */
-		float getForwardAcceleration();
+		Vector2D getForwardAcceleration() const;
 }

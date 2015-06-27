6280bfce928f38bb64ea92d5935ae9ae7ea6865e
Sherry Wu <code@shewu.me>
2015-06-27 15:04:15 -0700

    scaffold for AntiMitosis (inspired by agar.io)

---
+++ src/AntiMitosisGame.h
@@ -0,0 +1,38 @@
+#ifndef CTFGAME_H
+#define CTFGAME_H
+
+#include <map>
+
+#include "Game.h"
+#include "mapinfo/CTF.h"
+
+class CTFGame : public Game {
+public:
+    CTFGame(HBMap const& hbmap) : Game(hbmap) {
+        numplayers[0] = 0;
+        numplayers[1] = 0;
+        score[0] = 0;
+        score[1] = 0;
+    }
+
+private:
+    int numplayers[2];
+    int score[2];
+    std::map<GamePlayer*, int> teamLookup;
+
+    void doGameLogic();
+
+    RoundCollisionResult roundWallCollision(ObjectPtr<MovingRoundObject>,
+                                            ObjectPtr<Wall>);
+    std::pair<RoundCollisionResult, RoundCollisionResult> roundRoundCollision(
+        ObjectPtr<MovingRoundObject>, ObjectPtr<MovingRoundObject>);
+    GamePlayer* onPlayerAdded();
+    void onPlayerRemoved(GamePlayer*);
+    void onInit();
+    std::string getScore(GamePlayer*);
+
+    void createFlag(int regionNum);
+    void createNewPlayer(int teamNum, GamePlayer* gp);
+};
+
+#endif
